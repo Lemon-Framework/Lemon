dc2894a96f5bbb04532251b75c77b22d88dfef83
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-01-21 21:54:12 +0100

    init config

--- src/Lemon/Config/Config.php
+++ src/Lemon/Config/Config.php
@@ -1,6 +1,60 @@
 <?php
 
+
+namespace Lemon\Config;
+
+use Exception;
+use Lemon\Kernel\Lifecycle;
+
+/**
+ * Main interface for storing config data in organised way
+ *
+ * @method \Lemon\Config\Units\Init getInit() 
+ */
 class Config
 {
-    
+    /**
+     * Lifecycle config unit belongs to.
+     * 
+     * @var Lifecycle $lifecycle
+     */
+    public Lifecycle $lifecycle;
+
+    /**
+     * Array of all config units (components)
+     */
+    public array $units = [
+        'Init' => null
+    ];
+
+    /**
+     * Creates new config instance
+     *
+     * @param Lifecycle $lifecycle
+     */
+    public function __construct(Lifecycle $lifecycle)
+    {
+        $this->lifecycle = $lifecycle;
+        $this->loadUnits();
+    }
+
+    /**
+     * Loads all config units
+     *
+     * @return void
+     */
+    private function loadUnits(): void
+    {
+        $base = __NAMESPACE__;
+        foreach ($this->units as $unit => $_)
+            $this->units[$unit] = new ($base . $unit)($this);
+    } 
+
+    public function __call($name, $_)
+    {
+        if (preg_match('/get([A-Z][a-z]+)/', $name, $matches))
+            return $this->units[$matches[1]]; 
+
+        throw new Exception('Call to undefined method Config::' . $name . '()');
+    }
 }
---
+++ src/Lemon/Config/Units/Init.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Lemon\Config\Units;
+
+/**
+ * Initial configuration
+ *
+ * @property string $mode
+ * @property string $debug
+ */
+class Init extends Unit
+{
+    public function __construct()
+    {
+        $this->data = [
+            'mode' => LEMON_MODE,
+            'debug' => false 
+        ]; 
+    }
+} 
