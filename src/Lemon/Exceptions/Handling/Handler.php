982ddd6cc9c7f558626aca69e81a316f60417367
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-01-21 21:55:07 +0100

    init wip handling

---
+++ src/Lemon/Exceptions/Handling/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lemon\Exceptions\Handling;
+
+use Lemon\Kernel\Lifecycle;
+
+class Handler
+{
+
+    public readonly mixed $problem;
+
+    public readonly Lifecycle $lifecycle;
+
+    public function __construct($problem, Lifecycle $lifecycle)
+    {
+        $this->problem = $problem; 
+        $this->lifecycle = $lifecycle;
+    }
+    
+    /**
+     * Executes handler depending on debug settings
+     */
+    public function terminate()
+    {
+        if ($this->lifecycle->config('init', 'debug'))
+            echo $this->problem; // TODO REPORTER
+        else
+            raise(500)->terminate();
+    }
+}
