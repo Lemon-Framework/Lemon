c22d9c8eceba74344a570fdd77ce870b7244c14f
TENMAJKL <silder.hologram@gmail.com>
2022-08-03 13:16:14 +0200

    adds components tests

--- src/Lemon/Terminal/IO/Html/Components.php
+++ src/Lemon/Terminal/IO/Html/Components.php
@@ -31,7 +31,7 @@ class Components
     public function parseElement(DOMNode $element): string
     {
         if ($element instanceof DOMText) {
-            return $element->textContent;
+            return self::removeWhitespace($element);
         }
 
         $name = $element->nodeName;
@@ -52,28 +52,47 @@ class Components
     public function parseH1(DOMNode $element): string
     {
         $content = $this->parse($element);
-        $line = '+'.str_repeat('-', strlen($content) + 2).'+';
+        $line = '+'.str_repeat('-', self::lenght($content) + 2).'+';
 
-        return $line.PHP_EOL.'| '.$content.' |'.PHP_EOL.$line;
+        return $line.PHP_EOL.'| '.$content.' |'.PHP_EOL.$line.PHP_EOL;
     }
 
     public function parseHr(): string
     {
-        return str_repeat('-', 0); // TODO get size
+        return str_repeat('-', (int) exec('tput cols')); // TODO size
     }
 
     public function parseB(DOMNode $node): string
     {
-        return "\033[1m".$this->parse($node)."\033[0m";
+        return "\033[1m".$this->parse($node);
     }
 
     public function parseI(DOMNode $node): string
     {
-        return "\033[3m".$this->parse($node)."\033[0m";
+        return "\033[3m".$this->parse($node);
     }
 
     public function parseU(DOMNode $node): string
     {
-        return "\033[4m".$this->parse($node)."\033[0m";
+        return "\033[4m".$this->parse($node);
+    }
+
+    public static function lenght(string $target): int
+    {
+        return strlen(preg_replace("/\033\[[0-9]+m/", '', $target));
+    }
+
+    public static function removeWhitespace(DOMText $element): string
+    {
+        $content = $element->textContent;
+        if ($element->previousSibling === null) {
+            $content = ltrim($content);
+        }
+
+        if ($element->nextSibling === null) {
+            $content = rtrim($content);
+        }
+
+        return preg_replace("/(\n|\r)/", '', preg_replace('/\s{2,}/', ' ', $content));
     }
 }
--- tests/Terminal/IO/Html/ComponentsTest.php
+++ tests/Terminal/IO/Html/ComponentsTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Lemon\Tests\Terminal\IO\Html;
 
+use DOMDocument;
+use DOMNode;
+use Lemon\Terminal\IO\Html\Components;
 use Lemon\Tests\TestCase;
 
 /**
@@ -12,4 +15,88 @@ use Lemon\Tests\TestCase;
  */
 class ComponentsTest extends TestCase
 {
+    public function testParsingDiv()
+    {
+        $components = new Components();
+        $result = $components->parseElement($this->getElement('<div>parek v rohliku</div>'));
+        $this->assertSame("parek v rohliku\033[0m", $result);
+
+        $result = $components->parseElement($this->getElement('<div><div>parek v rohliku</div></div>'));
+        $this->assertSame("parek v rohliku\033[0m\033[0m", $result);
+
+        $result = $components->parseElement($this->getElement('<div class="bg-yellow">parek v rohliku</div>'));
+        $this->assertSame("\033[43mparek v rohliku\033[43m\033[0m", $result);
+
+        $result = $components->parseElement($this->getElement('<div class="bg-yellow">parek <div class="bg-red">v</div> rohliku</div>'));
+        $this->assertSame("\033[43mparek \033[43m\033[43m\033[41mv\033[41m\033[0m\033[43m\033[43m rohliku\033[43m\033[0m", $result);
+    }
+
+    public function testH1()
+    {
+        $components = new Components();
+        $result = $components->parseElement($this->getElement('<h1>parek</h1>'));
+        $this->assertSame(<<<HTML
+        +-------+
+        | parek\033[0m |
+        +-------+
+
+        HTML, $result);
+    }
+
+    public function testB() 
+    {
+        $components = new Components();
+        $result = $components->parseElement($this->getElement('<b>parek</b>'));           
+        $this->assertSame("\033[1mparek\033[0m", $result);
+    }
+
+    public function testI() 
+    {
+        $components = new Components();
+        $result = $components->parseElement($this->getElement('<i>parek</i>'));           
+        $this->assertSame("\033[3mparek\033[0m", $result);
+    }
+
+    public function testU() 
+    {
+        $components = new Components();
+        $result = $components->parseElement($this->getElement('<u>parek</u>'));           
+        $this->assertSame("\033[4mparek\033[0m", $result);
+    }
+
+    public function testLengt()
+    {
+        $this->assertSame(5, Components::lenght("\033[33mparek\033[0m"));
+        $this->assertSame(5, Components::lenght("pa\033[33m\033[31mrek"));
+        $this->assertSame(5, Components::lenght("parek"));
+    }
+
+    public function testParsing()
+    {
+        $components = new Components();
+        $dom = new DOMDocument();
+        $dom->loadHTML(<<<HTML
+        <h1>Something <b>cool</b></h1>
+        <div class="bg-yellow">
+            Very nice
+            <div class="bg-red">not so nice</div>
+            Nice again
+        </div>
+        HTML);
+
+        $this->assertSame(<<<HTML
+        +----------------+
+        | Something \033[1mcool\033[0m\033[0m |
+        +----------------+
+        \033[43mVery nice \033[43m\033[43m\033[41mnot so nice\033[41m\033[0m\033[43m\033[43m Nice again\033[43m\033[0m\033[0m
+        HTML, $components->parse($dom->getElementsByTagName('body')[0]));
+    }
+
+    private function getElement(string $el): DOMNode
+    {
+        $dom = new DOMDocument();
+        $dom->loadHTML($el);
+
+        return $dom->getElementsByTagName('body')[0]->firstChild;
+    }
 }
