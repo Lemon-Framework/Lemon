41527056dbda33c2039b0d8daa2d02eaed71ed97
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-09-09 19:21:05 +0200

    return of legend

--- src/Lemon/Support/helpers.php
+++ src/Lemon/Support/helpers.php
@@ -92,6 +92,16 @@ if (!function_exists('error')) {
     }
 }
 
+if (!function_exists('is_user_podvodnik')) {
+    /**
+     * Returns whenever is user podvodnik using sofisticated algorithms
+     */
+    function is_user_podvodnik(string $name): bool
+    {
+        return $name === 'CoolFido';
+    }
+}
+
 if (!function_exists('pipe')) {
     /**
      * Returns new Pipe.
