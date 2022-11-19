e69eec78804c73a83a60a7b715cc2d220343ae4d
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-11-19 19:27:17 +0100

    adds translating middleware

--- src/Lemon/Translating/Middlewares/TranslationLocalizer.php
+++ src/Lemon/Translating/Middlewares/TranslationLocalizer.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Lemon\Translating\Middlewares;
 
+use Lemon\Contracts\Http\Session;
+use Lemon\Contracts\Translating\Translator;
+
 class TranslationLocalizer
 {
-    
+    public function handle(Translator $translator, Session $session)
+    {
+        $translator->locate($session->get('locale'));
+    }    
 }
