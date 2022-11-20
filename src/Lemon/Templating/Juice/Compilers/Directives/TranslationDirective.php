91b548af8326e0e7dca2749204fa2b531c0d0c71
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-11-20 13:31:29 +0100

    tests

--- src/Lemon/Templating/Juice/Compilers/Directives/TranslationDirective.php
+++ src/Lemon/Templating/Juice/Compilers/Directives/TranslationDirective.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Lemon\Templating\Juice\Compilers\Directives;
 
+use Lemon\Templating\Exceptions\CompilerException;
 use Lemon\Templating\Juice\Token;
 
 class TranslationDirective implements Directive
 {
     public function compileOpenning(Token $token, array $stack): string
     {
-        return '<?php echo \Lemon\Translator::text(\''.$token->content.'\') ?>';
+        if ('' === $token->content[1]) {
+            throw new CompilerException('Directive text expects one argument');
+        }
+        return '<?php echo \Lemon\Translator::text(\''.$token->content[1].'\') ?>';
     } 
 }
---
+++ tests/Templating/Juice/Compilers/Directives/TranslationDirectiveTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lemon\Tests\Templating\Juice\Compilers\Directives;
+
+use Lemon\Templating\Exceptions\CompilerException;
+use Lemon\Templating\Juice\Compilers\Directives\TranslationDirective;
+use Lemon\Templating\Juice\Token;
+use Lemon\Tests\TestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+class TranslationDirectiveTest extends TestCase
+{
+    public function testOpen()
+    {
+        $c = new TranslationDirective();
+        $this->assertSame('<?php echo \Lemon\Translator::text(\'klobas\') ?>', $c->compileOpenning(new Token(Token::TAG, ['text', 'klobas'], 1), []));
+        $this->expectException(CompilerException::class);
+        $c->compileOpenning(new Token(Token::TAG, ['text', ''], 1), []);
+    }
+}
