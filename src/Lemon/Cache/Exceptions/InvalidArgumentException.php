c6445f3a05cf371c40f63390d45a3ca39d82faa6
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2023-09-16 16:24:14 +0200

    fixes Lemon\Cache\Exceptions\InvalidArgumentException

--- src/Lemon/Cache/Exceptions/InvalidArgumentException.php
+++ src/Lemon/Cache/Exceptions/InvalidArgumentException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Lemon\Cache\Exceptions;
 
-use Psr\Cache\InvalidArgumentException as PsrInvalidArgumentException;
+use Psr\SimpleCache\InvalidArgumentException as PsrInvalidArgumentException;
 
 class InvalidArgumentException extends CacheException implements PsrInvalidArgumentException
 {
