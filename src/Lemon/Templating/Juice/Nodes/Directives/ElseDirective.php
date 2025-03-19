39fffc170f2af805e6e347a5f772958e9800e8e6
TENMAJKL <silder.hologram@gmail.com>
2025-03-19 21:37:09 +0000

    fix

--- src/Lemon/Templating/Juice/Nodes/Directives/ElseDirective.php
+++ src/Lemon/Templating/Juice/Nodes/Directives/ElseDirective.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Lemon\Templating\Juice\Nodes\Directives;
 
+use Lemon\Templating\Juice\Generators;
 use Lemon\Templating\Juice\Nodes\SingletonDirective;
+use Lemon\Templating\Juice\SematicContext;
 
 class ElseDirective extends SingletonDirective
 {
